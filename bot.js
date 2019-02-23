50bd678491e21f491c5cb7409b1e5ebeac896d41
kinghilal2 <47920420+kinghilal2@users.noreply.github.com>
2019-02-23 22:01:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: $50
+    credits: 50
 };
 
 let userData = credits[message.author.id];

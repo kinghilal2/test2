484d88f9a7ef9178f5a93f30745054659f386b61
kinghilal2 <47920420+kinghilal2@users.noreply.github.com>
2019-02-23 21:50:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 's';
+const prefix = '$';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -207,7 +207,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
+    credits: $50
 };
 
 let userData = credits[message.author.id];
